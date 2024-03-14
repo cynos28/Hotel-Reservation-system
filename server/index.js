954150d2b11452bc69391ce885eeddf2ca5d6a30
tharusha01Bandara <154556642+tharusha01Bandara@users.noreply.github.com>
2024-03-15 03:07:43 +0530

    Create server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser"
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3001;
+const URL = process.env.MONGOURL;
+
+mongoose.connect(URL).then(()=>{
+
+    console.log("DB connected successfully");
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port: ${PORT}`);
+    })
+
+}).catch(error => console.log(error));
+
+
