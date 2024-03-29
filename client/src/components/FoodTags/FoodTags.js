ab9933ed3825048bb4e3b0b6d808a053a0f0caf4
Kavish_X <143331695+Kavishka-Rajapaksha@users.noreply.github.com>
2024-03-29 15:54:15 +0530

    tags

--- client/src/components/FoodTags/FoodTags.js
+++ client/src/components/FoodTags/FoodTags.js
@@ -1,13 +1,22 @@
 // import React from 'react';
 // import classes from './foodtags.module.css';
+// import { Link } from 'react-router-dom';
+
 
 // export default function FoodTags({tags, forFoodPage}) {
-//   return 
+//   return (
 //     <div className={classes.containeer}
 //     style={{
-//         justifyContent: forFoodPage?
-//     }}>
+//         justifyContent: forFoodPage? 'start' : 'center',
+//     }}
+//     >
+//         {tags.map (tag => (
+//             <Link key = {tag.mame} to = {`/foodpage/tag/${tag.name}`}>
+//                 {tag.name}
+//                 {!forFoodPage && `(${tag.count})`}
+//             </Link>
+//         ))}
         
 //     </div>
-  
+//   );
 // }
