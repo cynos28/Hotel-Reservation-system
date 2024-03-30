daa47209bfe127012149bac53ed8d732c5764657
tharusha01Bandara <154556642+tharusha01Bandara@users.noreply.github.com>
2024-03-30 15:39:54 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -13,15 +13,18 @@ import ChangePassword from './pages/ChangePassword/ChangePassword';
 import Events from './pages/events/events';
 import FoodPage from './pages/FoodPage/FoodPage';
 
+import AddRoom from './components/RoomComponents/addroom/Add';
+import GetRoom from './components/RoomComponents/getroom/Room';
+import RoomTable from './components/RoomComponents/getroom/Roomtable';
+import Edit from './components/RoomComponents/updateroom/Edit';
 
-
-import AdminLayout from "./AdminPanel/AdminComponents/AdminLayout/AdminLayout";
-import 'remixicon/fonts/remixicon.css';
-import AddRoom from './AdminPanel/AdminComponents/RoomComponents/addroom/Add';
-import GetRoom from './AdminPanel/Adminpages/AdminDashboard';
-import Edit from './AdminPanel/AdminComponents/RoomComponents/updateroom/Edit';
-import RoomTable from './AdminPanel/AdminComponents/RoomComponents/getroom/Roomtable';
-import Router from './AdminPanel/routes/Router';
+//import AdminLayout from "./AdminPanel/AdminComponents/AdminLayout/AdminLayout";
+//import 'remixicon/fonts/remixicon.css';
+//import AddRoom from './AdminPanel/AdminComponents/RoomComponents/addroom/Add';
+//import GetRoom from './AdminPanel/Adminpages/AdminDashboard';
+//import Edit from './AdminPanel/AdminComponents/RoomComponents/updateroom/Edit';
+//import RoomTable from './AdminPanel/AdminComponents/RoomComponents/getroom/Roomtable';
+//import Router from './AdminPanel/routes/Router';
 
 
 
@@ -47,13 +50,13 @@ function App() {
           {/* events */}
           <Route path='/events' element={<Events/>}/>
           
-<<<<<<< HEAD
           {/* <Route path='/dashboard' element={<GetRoom/>}/>
-          <Route path='/addroom' element={<AddRoom/>}/> */}
-=======
-           <Route path='/dashboard' element={<GetRoom/>}/>
-          <Route path='/addroom' element={<AddRoom/>}/>
->>>>>>> main
+          <Route path='/addroom' element={<AddRoom/>}/> */}\
+
+              <Route path="/getroom" element={<GetRoom />} />
+              <Route path="/addroom" element={<AddRoom />} />
+              <Route path="/roomtable" element={<RoomTable />} />
+              <Route path="/edit" element={<Edit />} />
           
              
 
@@ -65,10 +68,17 @@ function App() {
             <Layout> <ChangePassword /> </Layout>
           } />
 
-           {/* food page */}
+            food page 
            <Route path="/foodpage" element={<FoodPage/>}/>
            <Route path="/foodpage/search/:searchTerm" element={<FoodPage/>}/>
            <Route path="/foodpage/tag/:tag" element={<FoodPage/>}/>
+
+
+           
+           <Route path="/getroom" element={<GetRoom />} />
+            <Route path="/addroom" element={<AddRoom />} />
+              <Route path="/roomtable" element={<RoomTable />} />
+              <Route path="/edit" element={<Edit />} />
 
         </Routes>
         
