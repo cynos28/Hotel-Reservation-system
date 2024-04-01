fdecf593e8f640638457f63b885382222ca2ecd1
Cynos28 <sendtocynos@gmail.com>
2024-04-02 00:00:22 +0530

    store js

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -2,13 +2,30 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     isLoggedIn: false,
+    user:null,
+    user:[],
+    twoFactor:false,
+    isError:false,
+    isSuccess:false,
+    isLoading:false,
+    message:"",
 
-}
+
+};
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {}
+  reducers: {
+    RESET(state){
+        state.twoFactor = false;
+        state.isErro = false;
+        state.isSuccess = false;
+        state.isLoading = false;
+        state. message = "";
+
+    }
+  },
 });
 
 export const {} = authSlice.actions
