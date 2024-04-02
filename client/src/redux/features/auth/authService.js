be220e0b823c0a1b68a16b92db21b17f500b2c15
Cynos28 <sendtocynos@gmail.com>
2024-04-02 14:38:46 +0530

    auth slice and service

--- client/src/redux/features/auth/authService.js
+++ client/src/redux/features/auth/authService.js
@@ -2,3 +2,17 @@ import axios from "axios"
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 const API_URL = `${BACKEND_URL}/api/users/`
+
+//Register user
+const register = async (userData) =>{
+    const response = await axios.post(APU_URL + "register",userData);
+    return response.data;
+};
+
+const authService ={
+
+    register
+}
+
+
+export default authService;
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,4 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import {toast} from "react-toastify";
+import authService from './authService';
+
+
+
 
 const initialState = {
     isLoggedIn: false,
@@ -12,6 +17,26 @@ const initialState = {
 
 
 };
+//Register User
+export const register = createAsyncThunk(
+  "auth/register",
+  async (userData, thunkAPI) => {
+
+    try {
+      return await authService.register(userData);
+      
+    } catch (error) {
+      const message = (error.response && error.
+      response.data && error.response.data.
+      message) || error.message || error.toString();
+      
+      return thunkAPI.rejectWithValue(message);
+      
+    }
+
+
+  }
+)
 
 const authSlice = createSlice({
   name: "auth",
