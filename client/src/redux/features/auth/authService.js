b527500f5080134780840f50a70e593ec3fc5686
Cynos28 <sendtocynos@gmail.com>
2024-04-04 21:57:18 +0530

    already exist fixed

--- client/src/redux/features/auth/authService.js
+++ client/src/redux/features/auth/authService.js
@@ -6,11 +6,17 @@ const API_URL = `${BACKEND_URL}/api/users`;
 // Register user
 const register = async (userData) => {
     try {
-        const response = await axios.post(API_URL + "/register", userData);
+        const response = await axios.post(`${API_URL}/register`, userData);
         return response.data;
     } catch (error) {
-        console.error("Error registering user:", error);
-        throw new Error("Registration failed. Please try again.");
+        if (error.response &&  "User already exists with this email.") {
+            // Handle specific error for existing user
+            throw new Error("User already exists with this email.");
+        } else {
+            // Handle other errors
+            console.error("Error registering user:", error);
+            throw new Error("Registration failed. Please try again.");
+        }
     }
 };
 
