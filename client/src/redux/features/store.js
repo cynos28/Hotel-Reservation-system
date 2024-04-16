4c92a2295ba42eb42bb32657d15b66d4d775db63
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-04-17 00:08:55 +0530

    Redux persist added

--- client/src/redux/features/store.js
+++ client/src/redux/features/store.js
@@ -1,15 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice"
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
 import emailReducer from "../features/email/emailSlice";
 import filterReducer from "../features/auth/filterSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
+const reducers = combineReducers({
+  auth: authReducer,
+  email: emailReducer,
+  filter: filterReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer:{
-        auth:authReducer,
-        email: emailReducer,
-        filter: filterReducer,
-    }
-})
+  reducer: persistedReducer,
+});
 
-
+export const persistor = persistStore(store);
