b33f227ddf32e1d8bae48c544690a1bf69d3a39d
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-07 10:32:46 +0530

    set navigation extra facilities to payment page

--- client/src/pages/Extra/User/Summery/Summery.js
+++ client/src/pages/Extra/User/Summery/Summery.js
@@ -27,46 +27,51 @@ function Summary() {
 
   return (
     <div className="bk_book_sum">
-    <div className="summry_div">
-      <div  className="summry_div_box">
-        <h2 className="sum_topic_extra">Summary</h2>
-        <p className="sum_detil_extra">
-          <b>Booking ID:</b> {booking.extraid}
-        </p>
-        <p className="sum_detil_extra">
-          <b>Name:</b> {booking.name}
-        </p>
-        <p className="sum_detil_extra">
-          <b>Gmail: </b>
-          {booking.gmail}
-        </p>
-        <p className="sum_detil_extra">
-          <b>Your Facility:</b> {booking.gym === "true" && <span>Gym</span>}
-          {booking.pool === "true" && <span>Pool</span>}
-          {booking.bar === "true" && <span>Bar</span>}
-          {booking.spa === "true" && <span>Spa</span>}
-          {booking.vehicle === "true" && <span>Vehicle</span>}
-          {booking.dayplan === "true" && <span>Day Plan</span>}
-          {booking.specialday === "true" && <span>Special Day</span>}
-          {booking.petfriend === "true" && <span>Pet Friend</span>}
-        </p>
-        <p className="sum_detil_extra tot">
-          <b>Total:</b>
-          Rs.{booking.total}.00
-        </p>
-        {/* Add more booking details as needed */}
-        <button className="booknow_btn" onClick={() => window.print()}>
-          Download
-        </button>
-        <button  onClick={() => (window.location.href = "/payment")} className="booknow_btn">Pay</button>
-        <button
-          className="booknow_btn"
-          onClick={() => (window.location.href = "/bookingvalidate")}
-        >
-          My Booking
-        </button>
+      <div className="summry_div">
+        <div className="summry_div_box">
+          <h2 className="sum_topic_extra">Summary</h2>
+          <p className="sum_detil_extra">
+            <b>Booking ID:</b> {booking.extraid}
+          </p>
+          <p className="sum_detil_extra">
+            <b>Name:</b> {booking.name}
+          </p>
+          <p className="sum_detil_extra">
+            <b>Gmail: </b>
+            {booking.gmail}
+          </p>
+          <p className="sum_detil_extra">
+            <b>Your Facility:</b> {booking.gym === "true" && <span>Gym</span>}
+            {booking.pool === "true" && <span>Pool</span>}
+            {booking.bar === "true" && <span>Bar</span>}
+            {booking.spa === "true" && <span>Spa</span>}
+            {booking.vehicle === "true" && <span>Vehicle</span>}
+            {booking.dayplan === "true" && <span>Day Plan</span>}
+            {booking.specialday === "true" && <span>Special Day</span>}
+            {booking.petfriend === "true" && <span>Pet Friend</span>}
+          </p>
+          <p className="sum_detil_extra tot">
+            <b>Total:</b>
+            Rs.{booking.total}.00
+          </p>
+          {/* Add more booking details as needed */}
+          <button className="booknow_btn" onClick={() => window.print()}>
+            Download
+          </button>
+          <button
+            onClick={() => (window.location.href = "/add-Payment")}
+            className="booknow_btn"
+          >
+            Pay
+          </button>
+          <button
+            className="booknow_btn"
+            onClick={() => (window.location.href = "/bookingvalidate")}
+          >
+            My Booking
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
