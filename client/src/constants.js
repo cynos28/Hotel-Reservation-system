73e6b26aa1b25b906c3540801c583cc3791bde74
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-08 10:20:25 +0530

    constant change

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,12 +4,4 @@ export const PAYMENT_TYPES = {
   ROOM: "Room",
   FOOD: "Food",
   EXTRA: "Extra Services",
-  // POOL: "Pool",
-  // BAR: "Bar",
-  // GYM: "Gym",
-  // SPA: "Spa",
-  // VEHICLE: "Vehicle Services",
-  // DAY: "Day Planning",
-  // PET: "Pet Friendly",
-  // SPECIAL: "Special Day",
 };
